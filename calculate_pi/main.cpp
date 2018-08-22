95aa5554a32c2eb929686a4011b3ecd7d573bc05
oazucena <arcofblues@gmail.com>
2018-08-22 10:01:44 -0700

    Correctly use to hold temp data from each array. Next use critical regions or some kind of mutext for sum.

--- calculate_pi/main.cpp
+++ calculate_pi/main.cpp
@@ -18,23 +18,26 @@ int main()
         //Equation for pi is : sum(0,1,deltaX,F(x)*deltaX)
         //where F(x) = 4.0/(1+x*x)
         double start = omp_get_wtime();
-        int N = 10000000; // use 100 sub divs
-        double range = 1.0;
-        double deltaX = range/double(N);
+        const int N = 10000000; // use 100 sub divs
+        const double range = 1.0;
+        const double deltaX = range/double(N);
         double sum = 0;
-        auto F = [](double x)->double
+        const auto F = [](const double x)->double
         {
             return 4.0/(1.0 + x*x);
         };
         double x = deltaX/2;
         for(int c = 0; c < N; c++)
         {
-            sum += 4.0/(1.0 + x*x)*deltaX;
+            sum += 4.0/(1.0 + x*x);
             x += deltaX;
         }
-
+        sum *= deltaX;
         time_map[0] += omp_get_wtime() - start;
+        cout << "----------------------------------------" << endl;
+        cout << "Pi: " << sum << endl;
 
+        double *sum_array = new double[2];
         omp_set_num_threads(2);
         int part = N/2;
         sum = 0;
@@ -48,17 +51,23 @@ int main()
             double x_temp = part*id*temp_del + temp_del/2;
             for(int c = part*id; c < temp_N; c++)
             {
-                sum_temp += 4.0/(1.0 + x_temp*x_temp)*temp_del;
+                sum_temp += 4.0/(1.0 + x_temp*x_temp);
                 x_temp += temp_del;
             }
-            sum += sum_temp;
-            //printf("Id: %d\n", id);
+            sum_array[id] = sum_temp;
         }
+        for(int c =0; c < 2; c ++)
+        {
+            sum+=sum_array[c];
+        }
+        sum *= deltaX;
         time_map[2] += omp_get_wtime() - start;
-
+        cout << "Pi: " << sum << endl;
+        delete[] sum_array;
         omp_set_num_threads(4);
         part = N/4;
         sum = 0;
+        sum_array = new double[4];
         start = omp_get_wtime();
 #pragma omp parallel
         {
@@ -69,17 +78,24 @@ int main()
             double x_temp = part*id*temp_del + temp_del/2;
             for(int c = part*id; c < temp_N; c++)
             {
-                sum_temp += 4.0/(1.0 + x_temp*x_temp)*temp_del;
+                sum_temp += 4.0/(1.0 + x_temp*x_temp);
                 x_temp += temp_del;
             }
-            sum += sum_temp;
-            //printf("Id: %d\n", id);
+            sum_array[id] = sum_temp;
         }
+        for(int c =0; c < 4; c ++)
+        {
+            sum+=sum_array[c];
+        }
+        sum *= deltaX;
         time_map[4] += omp_get_wtime() - start;
+        cout << "Pi: " << sum << endl;
+        delete[] sum_array;
 
         omp_set_num_threads(8);
         part = N/8;
         sum = 0;
+        sum_array = new double[8];
         start = omp_get_wtime();
 #pragma omp parallel
         {
@@ -90,13 +106,19 @@ int main()
             double x_temp = part*id*temp_del + temp_del/2;
             for(int c = part*id; c < temp_N; c++)
             {
-                sum_temp += 4.0/(1.0 + x_temp*x_temp)*temp_del;
+                sum_temp += 4.0/(1.0 + x_temp*x_temp);
                 x_temp += temp_del;
             }
-            sum += sum_temp;
-            //printf("Id: %d\n", id);
+            sum_array[id] = sum_temp;
         }
+        for(int c =0; c < 8; c ++)
+        {
+            sum+=sum_array[c];
+        }
+        sum *= deltaX;
         time_map[8] += omp_get_wtime() - start;
+        cout << "Pi: " << sum << endl;
+        delete[] sum_array;
     }
 
     for (auto pair : time_map)
