e5e0fe5cd5201c17dc0acf70b53b7d264e097d2d
oazucena <arcofblues@gmail.com>
2018-08-22 23:25:25 -0700

    Created templated object to use OMP. The template parameter is an int constant that determines the number of threads at compile time. w

--- calculate_pi/main.cpp
+++ calculate_pi/main.cpp
@@ -2,123 +2,108 @@
 #include <iostream>
 #include <map>
 
+#include <functional>
+
 using namespace std;
+
+template<int N>
+class SimpleIntegrator
+{
+    const std::function<double(double)> &f;
+    size_t size;
+    double start;
+    double end;
+    double sum_array[N];
+    int part;
+public:
+    SimpleIntegrator(const std::function<int(int)> &f_in, const size_t &size_in, const double &start_in, const double &end_in) : f(f_in),
+        size(size_in),
+        start(start_in),
+        end(end_in),
+        part(size/N)
+    {
+    }
+    ~SimpleIntegrator()
+    {
+
+    }
+    double run()
+    {
+        omp_set_num_threads(N);
+        double temp_del = (end-start)/size;
+#pragma omp parallel
+        {
+            double sum_temp = 0.0;
+            int id = omp_get_thread_num();
+            int temp_N = part*(id+1);
+            double x_temp = part*id*temp_del + temp_del/2;
+            for(int c = part*id; c < temp_N; c++)
+            {
+                sum_temp += 4.0/(1.0 + x_temp*x_temp);
+                x_temp += temp_del;
+            }
+            sum_array[id] = sum_temp;
+        }
+        double sum = 0;
+        for(const auto & val : sum_array)
+        {
+            sum+=val;
+        }
+        sum *= temp_del;
+        return sum;
+    }
+};
+
 int main()
 {
     map<int,double> time_map
     {
-        {0,0.0f},
+        {1,0.0f},
         {2,0.0f},
         {4,0.0f},
         {8,0.0f},
     };
+
     int loop_total = 100;
+    //Equation for pi is : sum(0,1,deltaX,F(x)*deltaX)
+    //where F(x) = 4.0/(1+x*x)
+    const auto F = [](const double x)->double
+    {
+        return 4.0/(1.0 + x*x);
+    };
+
+    const size_t N = 10000000; // use 100 sub divs
+    const double begin = 0.0f;
+    const double end = 1.0f;
+
     for(int count = 0; count < loop_total; count++)
     {
-        //Equation for pi is : sum(0,1,deltaX,F(x)*deltaX)
-        //where F(x) = 4.0/(1+x*x)
+
+
         double start = omp_get_wtime();
-        const int N = 10000000; // use 100 sub divs
-        const double range = 1.0;
-        const double deltaX = range/double(N);
-        double sum = 0;
-        const auto F = [](const double x)->double
-        {
-            return 4.0/(1.0 + x*x);
-        };
-        double x = deltaX/2;
-        for(int c = 0; c < N; c++)
-        {
-            sum += 4.0/(1.0 + x*x);
-            x += deltaX;
-        }
-        sum *= deltaX;
-        time_map[0] += omp_get_wtime() - start;
+        SimpleIntegrator<1> integrator{F,N,begin,end};
+        double sum = integrator.run();
+        time_map[1] += omp_get_wtime() - start;
         cout << "----------------------------------------" << endl;
         cout << "Pi: " << sum << endl;
 
-        double *sum_array = new double[2];
-        omp_set_num_threads(2);
-        int part = N/2;
-        sum = 0;
         start = omp_get_wtime();
-#pragma omp parallel
-        {
-            double sum_temp = 0.0;
-            int id = omp_get_thread_num();
-            double temp_del = deltaX;
-            int temp_N = part*(id+1);
-            double x_temp = part*id*temp_del + temp_del/2;
-            for(int c = part*id; c < temp_N; c++)
-            {
-                sum_temp += 4.0/(1.0 + x_temp*x_temp);
-                x_temp += temp_del;
-            }
-            sum_array[id] = sum_temp;
-        }
-        for(int c =0; c < 2; c ++)
-        {
-            sum+=sum_array[c];
-        }
-        sum *= deltaX;
+        SimpleIntegrator<2> integrator2{F,N,begin,end};
+        sum = integrator2.run();
         time_map[2] += omp_get_wtime() - start;
         cout << "Pi: " << sum << endl;
-        delete[] sum_array;
-        omp_set_num_threads(4);
-        part = N/4;
-        sum = 0;
-        sum_array = new double[4];
+
         start = omp_get_wtime();
-#pragma omp parallel
-        {
-            double sum_temp = 0.0;
-            int id = omp_get_thread_num();
-            double temp_del = deltaX;
-            int temp_N = part*(id+1);
-            double x_temp = part*id*temp_del + temp_del/2;
-            for(int c = part*id; c < temp_N; c++)
-            {
-                sum_temp += 4.0/(1.0 + x_temp*x_temp);
-                x_temp += temp_del;
-            }
-            sum_array[id] = sum_temp;
-        }
-        for(int c =0; c < 4; c ++)
-        {
-            sum+=sum_array[c];
-        }
-        sum *= deltaX;
+        SimpleIntegrator<4> integrator4{F,N,begin,end};
+        sum = integrator4.run();
         time_map[4] += omp_get_wtime() - start;
         cout << "Pi: " << sum << endl;
-        delete[] sum_array;
 
-        omp_set_num_threads(8);
-        part = N/8;
-        sum = 0;
-        sum_array = new double[8];
         start = omp_get_wtime();
-#pragma omp parallel
-        {
-            double sum_temp = 0.0;
-            int id = omp_get_thread_num();
-            double temp_del = deltaX;
-            int temp_N =  part*(id+1);
-            double x_temp = part*id*temp_del + temp_del/2;
-            for(int c = part*id; c < temp_N; c++)
-            {
-                sum_temp += 4.0/(1.0 + x_temp*x_temp);
-                x_temp += temp_del;
-            }
-            sum_array[id] = sum_temp;
-        }
-        for(int c =0; c < 8; c ++)
-        {
-            sum+=sum_array[c];
-        }
-        sum *= deltaX;
+        SimpleIntegrator<8> integrator8{F,N,begin,end};
+        sum = integrator8.run();
         time_map[8] += omp_get_wtime() - start;
         cout << "Pi: " << sum << endl;
-        delete[] sum_array;
     }
 
     for (auto pair : time_map)
